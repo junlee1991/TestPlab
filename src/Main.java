12d322fe0182201c2e0f9df6cab5e45499611765
junlee1991 <julio_harjo@hotmail.sg>
2022-09-10 20:14:32 +0800

    +

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Main {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Main {
+    public static void main(String[] args){
+        // read
+        // write
+//        try {
+//            FileWriter fw = new FileWriter("dataduke.txt");
+//            fw.write("my dog is cute");
+//            fw.close();
+//        }catch(IOException e){
+//            e.printStackTrace();
+//        }
+//    }
 }
